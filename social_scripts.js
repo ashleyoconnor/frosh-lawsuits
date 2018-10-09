89fe53f8ea212d8e0e023416199543e5c1a79080
ashleyoconnor <43654080+ashleyoconnor@users.noreply.github.com>
2018-10-09 14:38:45 -0400

    updating meta tags

---
+++ social_scripts.js
@@ -0,0 +1,25 @@
+//These functions trigger share windows for Twitter and Facebook when a user clicks on an element assigned the noted class
+
+    $(".icon-twitter").on("click", function(){
+
+      var tweet = "xxx"; //Tweet text
+      var url = "http://cnsmaryland.org/interactives/fall-2018/frosh-lawsuits/index.html"; //Interactive URL
+
+      var twitter_url = "https://twitter.com/intent/tweet?text="+tweet+"&url="+url+"&tw_p=tweetbutton";
+      window.open(twitter_url, 'mywin','left=200,top=200,width=500,height=300,toolbar=1,resizable=0'); return false;
+
+    });
+
+
+    $(".icon-facebook").on("click", function(){
+
+      var picture = "xxx"; //Picture URL
+      var title = "xxx"; //Post title
+      var description = "xxx"; //Post description
+      var url = "http://cnsmaryland.org/interactives/fall-2018/frosh-lawsuits/index.html"; //Interactive URL
+
+        var facebook_url = "https://www.facebook.com/dialog/feed?display=popup&link="+url+"&picture="+picture+"&name="+title+"&description="+description+"&redirect_uri=http://www.facebook.com";
+      window.open(facebook_url, 'mywin','left=200,top=200,width=500,height=300,toolbar=1,resizable=0'); return false;
+
+    });
+// JavaScript Document
