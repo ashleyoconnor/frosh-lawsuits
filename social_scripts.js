9d8106fce5fb8013d102a9111cf5391fb822a522
ashleyoconnor <43654080+ashleyoconnor@users.noreply.github.com>
2018-10-11 09:50:02 -0400

    Updating social tags and social js

--- social_scripts.js
+++ social_scripts.js
@@ -2,7 +2,7 @@
 
     $(".icon-twitter").on("click", function(){
 
-      var tweet = "xxx"; //Tweet text
+      var tweet = "Maryland Attorney General Brian Frosh uses newfound legal authority to sue the federal government"; //Tweet text
       var url = "http://cnsmaryland.org/interactives/fall-2018/frosh-lawsuits/index.html"; //Interactive URL
 
       var twitter_url = "https://twitter.com/intent/tweet?text="+tweet+"&url="+url+"&tw_p=tweetbutton";
@@ -13,9 +13,9 @@
 
     $(".icon-facebook").on("click", function(){
 
-      var picture = "xxx"; //Picture URL
-      var title = "xxx"; //Post title
-      var description = "xxx"; //Post description
+      var picture = "http://cnsmaryland.org/interactives/fall-2018/facebook-thumb.psd"; //Picture URL
+      var title = "Frosh's broad legal power: Partisanship or 'long overdue'?"; //Post title
+      var description = "Maryland Attorney General Brian Frosh uses newfound legal authority to sue the federal government"; //Post description
       var url = "http://cnsmaryland.org/interactives/fall-2018/frosh-lawsuits/index.html"; //Interactive URL
 
         var facebook_url = "https://www.facebook.com/dialog/feed?display=popup&link="+url+"&picture="+picture+"&name="+title+"&description="+description+"&redirect_uri=http://www.facebook.com";
